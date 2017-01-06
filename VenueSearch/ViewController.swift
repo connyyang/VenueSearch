dad3e1480ea455fdcfc80577577243f99e8defec
Conny Yang <connyyang097@gmail.com>
2017-01-07 09:54:48 +1100

    Initial Commit

---
+++ VenueSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VenueSearch
+//
+//  Created by Conny Yang on 7/01/2017.
+//  Copyright © 2017 Conny Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
